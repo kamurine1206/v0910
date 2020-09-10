384683debdbc7b7bec05649ef34aa27f7a4a2a4d
yometo <blackymt@vip.qq.com>
2020-09-10 09:55:05 +0900

    復習

--- v0910/Form1.cs
+++ v0910/Form1.cs
@@ -12,9 +12,23 @@ namespace v0910
 {
     public partial class Form1 : Form
     {
+        int[] vx = new int[50];
+        int[] vy = new int[50];
+
         public Form1()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 50; i++)
+            {
+                vx[i] = rand.Next(-20, 21);
+                vy[i] = rand.Next(-20, 21);
+
+                labels[i] = new Label();
+                labels[i].AutoSize = true;
+                labels[i].Text = "☆";
+            }
+
         }
     }
 }
